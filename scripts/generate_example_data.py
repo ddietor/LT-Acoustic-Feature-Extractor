0576aa39a984e24dac0984e2505ccbc386e7b75e
ddietor <ddietor@icm.csic.es>
2026-06-13 11:05:34 +0200

    20260611_evening

--- scripts/generate_example_data.py
+++ scripts/generate_example_data.py
@@ -74,83 +74,73 @@ def run():
     # Create the output_results folder if not exists
     os.makedirs(args.output_results, exist_ok=True)
 
-    # # Generate example data
-    # config_signal = {
-    #     "duration": 300,                # [s]
-    #     "fs": 192000,                   # [Hz]
-    #     "earthquake_start": 5,          # [s]
-    #     "earthquake_duration": 35,      # [s]
-    #     "noise_std": 2.77e6,            # [uPa]
-    #     "earthquake_gain": 5.4,         # [a.u.] dB = 20 *np.log10(earthquake_gain)
-    #     "tone_50k_amplitude": 21,
-    #     "tone_100k_amplitude": 64,
-    #     "seed": 12,                     # Random seed for reproducibility   
-    # }
-
     config_signal = {
-        "duration": 300,                # [s]
-        "fs": 192000,                   # [Hz]
-        "fs_env": 100,                  # [Hz]
-        "noise_std": 2.77e6,            # [uPa]
-        "earthquake_amplitude": 14.06,  # [uPa]
-        "tone_50k_amplitude": 9.89e3,   # [uPa]
-        "tone_68k_amplitude": 8.94e3,   # [uPa]
-        "tone_75k_amplitude": 10.46e3,  # [uPa]
-        "seed": 12,                     # Random seed for reproducibility   
-    }
-    rng = np.random.default_rng(config_signal["seed"])
-    fs = config_signal["fs"]
-    duration = config_signal["duration"]
-    noise_std = config_signal["noise_std"]
-    N = int(fs * duration)
-    t = np.arange(N) / fs
-    noise_synthetic = rng.normal(loc=0,scale=noise_std,size=duration*fs)
-    tone50 = config_signal["tone_50k_amplitude"] * np.sin(2*np.pi*50e3*t + rng.uniform(0, 2*np.pi))
-    tone68 = config_signal["tone_68k_amplitude"] * np.sin(2*np.pi*68e3*t + rng.uniform(0, 2*np.pi))
-    tone75 = config_signal["tone_75k_amplitude"] * np.sin(2*np.pi*75e3*t + rng.uniform(0, 2*np.pi))
-    tones_signal = tone50 + tone68 + tone75
-    raw = rng.standard_normal(N)
-    sos = scp.signal.butter(4,[1, 40],btype="bandpass",fs=fs,output="sos")
-    seismic_base = scp.signal.sosfiltfilt(sos, raw)
-    seismic_base /= np.std(seismic_base)
-    earthquake = np.zeros(N)
-    envelope_read = np.loadtxt("/Users/ddietor/Documents/ddietor/LT-Acoustic-Feature-Extractor/scripts/envelope_smooth.txt")
-    fs_env = config_signal["fs_env"]
-    t_env = np.arange(len(envelope_read)) / fs_env
-    envelope = np.interp(t, t_env, envelope_read,left=0.0,right=0.0)
-    earthquake_uPa = config_signal["earthquake_amplitude"]
-    earthquake = envelope * seismic_base * earthquake_uPa
-    signal_uPa = (earthquake + noise_synthetic + tones_signal)
-
-    # Genera una senyal en amplitud creixent en freqüència, just jo esperava el contrari (pink noise as underwater noise)
-
-    # noise = config_signal["noise_std"] * rng.standard_normal(N)
-    # idx_start = int(config_signal["earthquake_start"] * fs)
-    # idx_end = int((config_signal["earthquake_start"] + config_signal["earthquake_duration"]) * fs)
-    # idxs_len = idx_end - idx_start
-    # raw_eq = rng.standard_normal(idxs_len)
-    # def lowpass(signal, cutoff, fs, order=4):
-    #     sos = scp.signal.butter(order, cutoff, btype="low", fs=fs, output="sos")
-    #     return scp.signal.sosfiltfilt(sos, signal)
-    # raw_eq = lowpass(raw_eq, 31, fs)
-    # raw_eq = raw_eq / np.std(raw_eq) # Normalize to unit std
-    # x = np.linspace(0, 1, idxs_len)
-    # envelope = np.exp(-3*x)   # decaimiento tipo coda
-    # envelope[:int(0.2*len(x))] *= np.linspace(0,1,int(0.2*len(x)))  # attack
-    # envelope = envelope / np.max(envelope)
-    # earthquake = np.zeros(N)
-    # earthquake_segment = (config_signal["earthquake_gain"]*envelope*raw_eq)
-    # earthquake[idx_start:idx_end] = earthquake_segment
-    # tone50 = config_signal["tone_50k_amplitude"]*np.sin(2*np.pi*50e3*t)
-    # tone100 = config_signal["tone_100k_amplitude"]*np.sin(2*np.pi*100e3*t)
-    # signal_uPa = noise + earthquake + tone50 + tone100
-
+            "duration": 300,          # [s]
+            "fs": 192000,             # [Hz]
+            "fs_env": 250,            # [Hz]
+            "noise_amp": 2.77e6,      # [uPa]
+            "earthquake_ini": 5,      # [s]
+            "earthquake_amp": 14.06e6,  # [uPa]
+            "tone_50k_amp": 9.89e3,   # [uPa]
+            "tone_68k_amp": 8.94e3,   # [uPa]
+            "tone_75k_amp": 10.46e3,  # [uPa]
+            "seed": 12,                     # Random seed for reproducibility   
+        }
+    
     factor_scale = 0.375*np.sqrt(2)
     Nbits = 24                      # ACDC
     RVR_hydro = -172                # Received Voltage Response [dB re 1uPa/V]
     Pref = 1e-6                     # Ref. pressure [Pa]
     Vref = 1                        # Ref. voltage [V]
     FS_uPa = factor_scale * (Pref / Vref) * (1 / 10**(RVR_hydro/20)) * 1e6 #Full Scale in uPa
+
+    rng = np.random.default_rng(config_signal["seed"])
+    fs = config_signal["fs"]
+    duration = config_signal["duration"]
+    Nsamples = int(fs * duration)
+    t = np.arange(Nsamples) / fs
+    white_noise= rng.normal(0,1,Nsamples)
+
+    Xfft_white = np.fft.rfft(white_noise)
+    white_f = np.fft.rfftfreq(Nsamples, 1/fs)
+    white_f[0] = white_f[1]
+    Xfft_pink = Xfft_white / np.sqrt(white_f)
+    pink_noise = np.fft.irfft(Xfft_pink, n=Nsamples)
+    pink_noise = pink_noise / np.max(np.abs(pink_noise))
+
+    tone50 = np.sin(2*np.pi*50e3*t + rng.uniform(0, 2*np.pi)) * config_signal["tone_50k_amp"] / config_signal["tone_50k_amp"]
+    tone68 = np.sin(2*np.pi*68e3*t + rng.uniform(0, 2*np.pi)) * config_signal["tone_68k_amp"] / config_signal["tone_68k_amp"]
+    tone75 = np.sin(2*np.pi*75e3*t + rng.uniform(0, 2*np.pi)) * config_signal["tone_75k_amp"] / config_signal["tone_75k_amp"]
+    tones_signal = tone50 + tone68 + tone75
+    tones_signal /= 3.0
+
+    envelope_read = np.loadtxt("./scripts/earthqueake_envelope.txt")
+    fs_env = config_signal["fs_env"]
+    t_env = np.arange(len(envelope_read)) / fs_env
+    t_env_fs = np.arange(0, t_env[-1], 1 / fs) 
+    envelope = np.interp(t_env_fs, t_env, envelope_read,left=0.0,right=0.0)
+    envelope = envelope[int(t_env_fs[0]+1 * fs):int(t_env_fs[-1]-1 * fs)]
+    envelope = envelope / np.max(np.abs(envelope))
+    # t_env_fs = t_env_fs[int(t_env_fs[0]+1 * fs):int(t_env_fs[-1]-1 * fs)]
+
+    earthquake_noise = rng.standard_normal(len(envelope))
+    sos = scp.signal.butter(4,[1, 40],btype="bandpass",fs=fs,output="sos")
+    earthquake_noise = scp.signal.sosfiltfilt(sos, earthquake_noise)
+    earthquake_noise = earthquake_noise / np.max(np.abs(earthquake_noise))
+    earthquake = envelope * earthquake_noise 
+    earthquake = earthquake / np.max(np.abs(earthquake))
+    earthquake *= (config_signal["earthquake_amp"] / FS_uPa)
+
+    ToA_earthquake = config_signal["earthquake_ini"]
+    Nini = int(ToA_earthquake * fs)
+    Nend = Nini + len(earthquake)
+
+    signal_rel = pink_noise.copy()
+    signal_rel[Nini:Nend] += earthquake
+    signal_rel += tones_signal
+    signal_rel /= np.max(np.abs(signal_rel))
+    # signal_uPa = signal_rel * FS_uPa 
+    signal_uPa = signal_rel
 
     metadata = {
         "description": "Signal Test",
